f58574d99b03362e65a4bc3ddc4aa2b1caa97d2f
Karl <kdirzys@NN121-HA-17.HS.sd203.org>
2015-08-24 10:29:38 -0500

    Made a new type of class called HelloPrinter

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -6,8 +6,17 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        turtle.setPenColor(Color.BLUE);
         turtle.penDown();
         turtle.forward(50);
+        turtle.turn(78);
+        turtle.forward(10);
+        for (;1>0;){
+            turtle.forward(10);
+            turtle.turn(2);
+            
+        }
+       
+        
     }
 }
