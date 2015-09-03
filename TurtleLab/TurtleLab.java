fd2942559313f947b09a5ff9999f6503fa8edf8c
Karl <kdirzys@NN121-HA-17.HS.sd203.org>
2015-09-03 10:25:20 -0500

    Turtle Lab Experimenting with Turtles

--- TurtleLab/TurtleLab.java
+++ TurtleLab/TurtleLab.java
@@ -3,27 +3,37 @@ public class TurtleLab
 {
     public static void main(String [] args)
     {
-        World lab = new World();
+        World lab = new World(1920,1080);
         Turtle turtle = new Turtle(lab);
         Turtle turtle2 = new Turtle(lab);
         turtle.setPenColor(Color.BLUE);
-        turtle2.setPenColor(Color.BLACK);
+        turtle2.setPenColor(Color.GREEN);
         turtle.penDown();
         turtle2.penDown();
         turtle.forward(50);
         turtle2.turn(78);
-        
-        for (;;)
+        int turn = 0;
+        while(true)
         {
-            turtle.forward(40);
-            turtle.turn(-15);
-             
-        
-        
-        
-          turtle2.forward();
-          turtle2.turn(90);
-            
+          turtle.forward(100);
+          turtle.turn(90);
+          //turtle.forward(30);
+          turtle.turn(29);
+          //turtle.forward(10);
+          // turtle.turn(200);
+          //turtle.forward(30);
+          //turtle.turn(-190);
+          
+          turtle2.forward(150); 
+          turn += 90;
+          turtle2.turn(turn);
+          turtle2.turn(10);
+          turtle2.penUp();
+          turtle2.forward(100);
+          turtle2.penDown();
+          turtle2.turn(turn);
+          turtle2.turn(66);
+      
             
         }
     }    
